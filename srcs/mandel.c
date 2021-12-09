df1047fd77bf2984d946192aad6f323de96700ff
laird-ikar <bguyot@student.42mulhouse.fr>
2021-12-09 13:29:02 +0100

    adding a gestion of Julia set + comments

--- srcs/mandel.c
+++ srcs/mandel.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 23:21:38 by bguyot            #+#    #+#             */
-/*   Updated: 2021/12/09 00:24:07 by bguyot           ###   ########.fr       */
+/*   Updated: 2021/12/09 13:26:47 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,39 @@
 
 int	main(void)
 {
+	if (JULIA)
+	{
+		julia();
+	}
+	else
+	{
+		mandel();
+	}
+}
+
+/*
+** mandel is the version where we fixes z and iterate for c
+*/
+void	mandel(void)
+{
 	t_complex	step;
 	t_complex	c;
+	t_complex	z;
 	int			i[2];
 
-	step.x = (MAX_X - MIN_X) / RESOLUTION;
+	step.x = (MAX_X - MIN_X) / RESOLUTION / RATIO;
 	step.y = (MAX_Y - MIN_Y) / RESOLUTION;
 	i[0] = 0;
 	c.y = step.y + MIN_Y;
-	while (i[0]++ < RESOLUTION)
+	while (i[0]++ <= RESOLUTION)
 	{
 		c.x = step.x + MIN_X;
 		i[1] = 0;
-		while (i[1]++ < RESOLUTION)
+		while (i[1]++ <= RESOLUTION * RATIO)
 		{
-			check_c(c);
+			z.x = 0;
+			z.y = 0;
+			check_diver(c, z);
 			c.x += step.x;
 		}
 		ft_putchar('\n');
@@ -36,18 +54,59 @@ int	main(void)
 	}
 }
 
-void	check_c(t_complex c)
+/*
+** julia is the version where we fixes c and iterate for z
+*/
+void	julia(void)
 {
+	t_complex	step;
+	t_complex	c;
 	t_complex	z;
-	int			i;
+	int			i[2];
 
-	z.x = 0;
-	z.y = 0;
+	step.x = (MAX_X - MIN_X) / RESOLUTION / RATIO;
+	step.y = (MAX_Y - MIN_Y) / RESOLUTION;
+	c.x = C_X;
+	c.y = C_Y;
+	i[0] = 0;
+	z.y = step.y + MIN_Y;
+	while (i[0]++ <= RESOLUTION)
+	{
+		z.x = step.x + MIN_X;
+		i[1] = 0;
+		while (i[1]++ <= RESOLUTION * RATIO)
+		{
+			check_diver(c, z);
+			z.x += step.x;
+		}
+		ft_putchar('\n');
+		z.y += step.y;
+	}
+}
+
+/*
+** check_diver is the function in wich we calculte the sequence
+**		z(0)		= z
+**		z(n + 1)	= z(n)^2 + c
+**	and then print the corresponding character
+**		'#' if the serie is borned
+**		' ' otherwise
+**	we check if the modulus of z is >= 2
+**		(aka z_x^2 + z_y^2 >= 4, to spare a sqrt)
+**	because we know for a fact that any number of modulus >= 2 diverges for that
+**	function
+*/
+void	check_diver(t_complex c, t_complex z)
+{
+	int			i;
+	double		temp_x;
+
 	i = 0;
 	while (i++ < PRECISION && z.x * z.x + z.y * z.y < 4)
 	{
-		z.x = z.x * z.x - z.y * z.y + c.x;
+		temp_x = z.x * z.x - z.y * z.y + c.x;
 		z.y = 2 * z.x * z.y + c.y;
+		z.x = temp_x;
 	}
 	if (z.x * z.x + z.y * z.y >= 4)
 		ft_putchar(' ');
--- srcs/mandel.h
+++ srcs/mandel.h
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 23:21:52 by bguyot            #+#    #+#             */
-/*   Updated: 2021/12/09 00:24:50 by bguyot           ###   ########.fr       */
+/*   Updated: 2021/12/09 13:21:48 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,53 @@
 
 # include "libft.h"
 
-# define RESOLUTION 60.0
-# define PRECISION 25.0
-# define MIN_X -2
-# define MAX_X 1.4
-# define MIN_Y -1.2
-# define MAX_Y 1.2
+/*
+** Parameters
+** RESOLUTION is the number of lines we output
+** PRECISION is the number of iteration we do
+** MIN_X, MAX_X, MIN_Y, MAX_Y are the borders in which we calculate
+** RATIO is the ratio we use to print a set that looks orthonormal
+*/
+# define RESOLUTION 300.0
+# define PRECISION 1000.0
+# define MIN_X -2.0
+# define MAX_X 2.0
+# define MIN_Y -2.0
+# define MAX_Y 2.0
+# define RATIO 2.1
 
+/*
+** More parameter for calculating Julia Sets
+** JULIA is a boolean
+**	JULIA = 1 -> Julia Set
+**	JULIA = 0 -> Mandelbrot Set
+** C_X and C_Y are the Real and Imaginary part of c
+*/
+# define JULIA 1
+# define C_X 0.3
+# define C_Y 0.5
+
+/*
+** A structure to have complex numbers
+*/
 typedef struct s_complex
 {
 	double	x;
 	double	y;
 }	t_complex;
 
-void	check_c(t_complex c);
+/*
+** check_diver is the function in wich we calculte the sequence
+**		z(0)		= z
+**		z(n + 1)	= z(n)^2 + c
+**	and then print the corresponding character
+**		'#' if the serie is borned
+**		' ' otherwise
+** julia is the version where we fixes c and iterate for z
+** mandel is the version where we fixes z and iterate for c
+*/
+void	check_diver(t_complex c, t_complex z);
+void	julia(void);
+void	mandel(void);
 
 #endif
